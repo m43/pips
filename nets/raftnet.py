e82e9fef1bd3a25e76c4ac240b52be09b1365e71
m43 <frano.rajich@gmail.com>
2023-03-16 01:08:17 +0100

    Make RAFT work with DP.

--- nets/raftnet.py
+++ nets/raftnet.py
@@ -38,11 +38,11 @@ class Raftnet(nn.Module):
         padder = InputPadder(image1.shape)
         image1, image2 = padder.pad(image1, image2)
         if test_mode:
-            flow_low, flow_up, feat = self.model(image1, image2, iters=iters, test_mode=test_mode)
+            flow_low, flow_up, feat = self.model(image1=image1, image2=image2, iters=iters, test_mode=test_mode)
             flow_up = padder.unpad(flow_up)
             return flow_up, feat
         else:
-            flow_predictions = self.model(image1, image2, iters=iters, test_mode=test_mode)
+            flow_predictions = self.model(image1=image1, image2=image2, iters=iters, test_mode=test_mode)
             return flow_predictions
     
         # print(flow_up.shape)
